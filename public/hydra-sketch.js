2ae004489520a16bddd25518a837dd2eab2b2665
Abhinay Khoparzi <abhinayk@gmail.com>
2020-12-03 02:47:28 +0530

    Got Hydra running and picking up the q5 canvas for effects

--- public/hydra-sketch.js
+++ public/hydra-sketch.js
@@ -1,16 +1,33 @@
-var hydra = new Hydra({
-    canvas: document.getElementById("hydraCanvas")
+let hydra, hydraCanvas
+// hydraCanvas = document.createElement("canvas")
+// hydraCanvas.width = 512;
+// hydraCanvas.height = 512;
+// hydraCanvas.id = "hydraCanvas";
+hydraCanvas = document.getElementById("hydraCanvas")
+hydraCanvas.width = window.innerWidth - 20;
+hydraCanvas.height = window.innerHeight - 20;
+hydra = new Hydra({
+    canvas: hydraCanvas,
+    makeGlobal: false,
+    detectAudio: false,
+    width: 512,
+    height: 512,
+})//
+p5 = document.getElementById("sketchCanvas")
+let visFX = hydra.synth
+visFX.s0.init({
+    src: p5.canvas,
+    dynamic: true // optional parameter. Set to false if using a static image or something that will not change
 })
-var audioContext = Tone.context;
-hydra.setResolution(1920, 1080)
-shape(() => (Math.sin(time * 0.00125) * 3.5 + 3.5), 0.25).repeat(4, 4).scrollY(0, -0.0125).out(o1)
-src(o1)
-    .add(
-        src(o1)
-        .scrollX(0.25 * 0.25)
-        .scrollY(0, 0.0125).scale(a.fft[2])
-    )
-    .kaleid(() => (Math.sin(time * 0.0001) * 6 + 3)).blend(src(o0).scale(0.75).kaleid(), 0.9)
-    .diff(src(o0).scale(0.9).luma(() => a.fft[2] * 1.7))
-    .scrollY(() => (Math.sin(time) * 0.01)).rotate(0.012)
-    .contrast(1).out()
+
+// 13 Nov 2020
+visFX.src(visFX.s0) // This should pick up the canvas element from p5
+// visFX.voronoi()
+// visFX.solid().add(visFX.src(visFX.s0), 1)
+    .modulateKaleid(visFX.voronoi(5, 0.01), [3, 9, 12].smooth().fast(0.125)).rotate(0, -0.1)
+    .blend(visFX.src(visFX.o1).scrollY(0.001).scale(0.999), 0.99)
+    // .color(0, 0, 3).colorama(0.4)
+    .out(visFX.o1)
+// var shader = visFX.src(visFX.o1).add(visFX.src(visFX.s0)).glsl()
+// console.log(shader)
+visFX.src(visFX.o1).add(visFX.src(visFX.s0)).out()
--- public/q5-sketch.js
+++ public/q5-sketch.js
@@ -4,18 +4,18 @@ var bgColor = '#0c1a21'
 var lastTransmittedPos = {}
 
 let q5 = new Q5();
+
 q5.setup = function () {
     // if (window.innerHeight < HEIGHT) {
-    //     HEIGHT = window.innerHeight * 0.7
-    // }
-    // WIDTH = window.innerWidth - 420
-
+        //     HEIGHT = window.innerHeight * 0.7
+        // }
+        // WIDTH = window.innerWidth - 420
+        
     q5.createCanvas(WIDTH, HEIGHT)
     q5.frameRate(30)
-    q5.createGraphics(WIDTH, HEIGHT)
     q5.background(bgColor)
     q5.colorMode(q5.HSL);
-
+    
     q5.textFont('Helvetica');
     q5.textSize(16);
     q5.textAlign(q5.CENTER, q5.CENTER);
@@ -23,7 +23,7 @@ q5.setup = function () {
 
 q5.draw = function () {
     q5.background(bgColor)
-
+    
     // blink effect for each dot starts below
     Object.keys(usersPos).forEach(i => {
         let r = 0
@@ -33,7 +33,7 @@ q5.draw = function () {
         } else {
             r = 10 + 100 * Math.sin((Math.PI / 100) * playedSince)
         }
-
+        
         let h = 20
         if (userId === Number(i)) {
             h = 70
@@ -42,20 +42,20 @@ q5.draw = function () {
         q5.circle(usersPos[i].x * WIDTH, usersPos[i].y * HEIGHT, r);
         // q5.text(usersPos[i].x + ' x ' + usersPos[i].y, usersPos[i].x * WIDTH, usersPos[i].y * HEIGHT);
     })
-
+    
     // movement
     if (q5.keyIsDown(q5.UP_ARROW)) {
         usersPos[userId].y = Math.max(usersPos[userId].y - 10 / HEIGHT, 0)
     }
-
+    
     if (q5.keyIsDown(q5.DOWN_ARROW)) {
         usersPos[userId].y = Math.min(usersPos[userId].y + 10 / HEIGHT, 1)
     }
-
+    
     if (q5.keyIsDown(q5.LEFT_ARROW)) {
         usersPos[userId].x = Math.max(usersPos[userId].x - 10 / WIDTH, 0)
     }
-
+    
     if (q5.keyIsDown(q5.RIGHT_ARROW)) {
         usersPos[userId].x = Math.min(usersPos[userId].x + 10 / WIDTH, 1)
     }
@@ -67,20 +67,25 @@ setInterval(() => {
     let hasXYChanged = false;
     let areKeysDown = false;
     areKeysDown = q5.keyIsDown(q5.UP_ARROW) || q5.keyIsDown(q5.DOWN_ARROW) || q5.keyIsDown(q5.LEFT_ARROW) || q5.keyIsDown(q5.RIGHT_ARROW);
-    hasXYChanged = (usersPos[userId].x != lastTransmittedPos.x) || (usersPos[userId].y != lastTransmittedPos.y)
-
+    // if (usersPos[userId]) {
+        hasXYChanged = (usersPos[userId].x != lastTransmittedPos.x) || (usersPos[userId].y != lastTransmittedPos.y)
+    // }
+    
     if (!areKeysDown && hasXYChanged) {
         const userXY = usersPos[userId];
         audio.onPositionChange(userXY, {
             x: q5.mouseX,
             y: q5.mouseY
         })
-
+        
         socket.onPositionChange(userXY);
-
+        
         lastTransmittedPos = {
             x: usersPos[userId].x,
             y: usersPos[userId].y
         }
     }
 }, 1000)
+
+let parent = document.getElementById("sketchCanvas");
+parent.appendChild(q5.canvas);
