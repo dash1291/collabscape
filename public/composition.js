bd6fd7cbb59bac8d0dd9c0ee89575b63de15e269
Abhinay Khoparzi <abhinayk@gmail.com>
2020-12-14 18:46:47 +0530

    Compositions for more tracks

--- public/composition.js
+++ public/composition.js
@@ -4,7 +4,7 @@ var sampleCount = 13;
 let sequences = ["0 3", "~ 5 ~", "12 ~ 7 ~ 8", "24 24 ~"];
 let pattern = [0, 2, 4, 5, 7, 9, 11];
 let tracks = [];
-let trackCount = 8;
+let trackCount = 20;
 
 var composition = {}
 
@@ -31,9 +31,31 @@ composition.startComposition = function(instrumentName) {
         tracks[index].synth = instrument.synth;
         tracks[index].panner = instrument.panner;
         tracks[index].addSequence(sequences[modulo(index, 4)]);
+        switch (index) {
+            case 4:
+                tracks[index].transpose = -12;
+                break;
+            case 5:
+                tracks[index].transpose = 24;
+                break;
+            case 6:
+                tracks[index].transpose = 12 + 7;
+                break;
+            case 7:
+                tracks[index].transpose = 24 + 7;
+                break;
+            case 8:
+                tracks[index].phase = 0.25;
+                break;
+            case 9:
+                tracks[index].phase = 0.5;
+                break;
+            default:
+                tracks[index].phase = 0.06125 * modulo(index, 4);
+                break;
+        }
     }
     
-
     // tracks[1].loadScale(tunerRand());
     // tracks[1].addPattern(pattern);
     // tracks[1].addSequences(sequences);
